52d49c007cc51213a044403f1ad83367c1d12e87
Marcello Esposito <esposito.marce@gmail.com>
2024-12-09 22:36:40 +0100

    Chores

--- src/backend/src/InnovaPrev/Controllers/GetQuoteController.cs
+++ src/backend/src/InnovaPrev/Controllers/GetQuoteController.cs
@@ -1,8 +1,6 @@
 ﻿using DomainModel.Classes;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace InnovaPrev.Controllers
 {
     [Route("api/getQuote")]
