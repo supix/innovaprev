1f0add93a2a46408e99980a7cbe8ee63c47dc03f
Marcello Esposito <esposito.marce@gmail.com>
2024-12-09 22:34:50 +0100

    POST api/pdf method available
    It returns a sample pdf file with content type `application/pdf`.

---
+++ src/backend/src/InnovaPrev/Controllers/PdfController.cs
@@ -0,0 +1,21 @@
+﻿using DomainModel.Classes;
+using Microsoft.AspNetCore.Mvc;
+using PdfQuote;
+
+namespace InnovaPrev.Controllers
+{
+    [Route("api/pdf")]
+    [ApiController]
+    public class PdfController : ControllerBase
+    {
+        // POST api/<PdfController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Project project)
+        {
+            var pdfGenerator = new Generator();
+            var buffer = pdfGenerator.Generate(project);
+            var streamResult = new MemoryStream(buffer);
+            return File(streamResult, "application/pdf", $"preventivo{ DateTime.Now.ToString("_yyyyMMdd_HHmmss") }.pdf");
+        }
+    }
+}
