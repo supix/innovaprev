448dc3c0248700ddb439be94d2d62b10cc2f6e81
Marcello Esposito <esposito.marce@gmail.com>
2025-01-06 15:31:16 +0100

    feat(backend): new `api/productImage` api available
    The api is called with the following syntax:
    
      `api/productCode/<productCode>?isThumb=<true|false>`
    
    The api returns a `image/jpeg` content type.

---
+++ src/backend/src/InnovaPrev/Controllers/ProductImageController.cs
@@ -0,0 +1,26 @@
+﻿using DomainModel.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InnovaPrev.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImageController : ControllerBase
+    {
+        private readonly IProductImageProvider productImageProvider;
+
+        public ProductImageController(IProductImageProvider productImageProvider)
+        {
+            this.productImageProvider = productImageProvider ?? throw new ArgumentNullException(nameof(productImageProvider));
+        }
+
+        // GET api/<ProductImageController>/IPC?isThumb=true
+        [HttpGet("{id}")]
+        public IActionResult Get(string id, bool isThumb)
+        {
+            return File(this.productImageProvider.Get(id, isThumb), "image/jpeg");
+        }
+    }
+}
