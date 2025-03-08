c505a41325736f0d6977ed0b642c5bf070f08362
Marcello Esposito <esposito.marce@gmail.com>
2025-03-08 22:25:32 +0100

    chores(backend): tests directly call product constructor
    instead of calling the factory

--- src/backend/src/DomainModel_Test/TestPrices.cs
+++ src/backend/src/DomainModel_Test/TestPrices.cs
@@ -1,5 +1,6 @@
 using DomainModel.Classes.Materials.ConcreteMaterials;
 using DomainModel.Classes.Products;
+using DomainModel.Classes.Products.ConcreteProducts;
 
 namespace DomainModel_Test
 {
@@ -13,7 +14,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_WoodWithCasAllowedLength_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("ELA");
+            var p = new ELA();
             var cas = new CAS(2500L);
             var price = p.GetMaterialPrice(cas);
             Assert.That(Math.Abs(price - 285 * 2.5M), Is.LessThan(1e-3M));
@@ -22,7 +23,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_PvcWithCasAllowedLength_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("SP");
+            var p = new SP();
             var cas = new CAS(2500L);
             var price = p.GetMaterialPrice(cas);
             Assert.That(Math.Abs(price - 268 * 2.5M), Is.LessThan(1e-3M));
@@ -31,7 +32,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_WoodWithCasLowLength_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("ELA");
+            var p = new ELA();
             var cas = new CAS(500L);
             var price = p.GetMaterialPrice(cas);
             Assert.That(Math.Abs(price - 285M), Is.LessThan(1e-3M));
@@ -40,7 +41,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_PvcWithCasLowLength_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("IPC");
+            var p = new IPC();
             var cas = new CAS(500L);
             var price = p.GetMaterialPrice(cas);
             Assert.That(Math.Abs(price - 268M), Is.LessThan(1e-3M));
@@ -49,7 +50,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_WoodWithDoubleAllowedLength_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("ELA");
+            var p = new ELA();
             var f1a = new F1A(2000L, 3000L);
             var price = p.GetMaterialPrice(f1a);
             Assert.That(Math.Abs(price - (985 + 38) * 2M * 3M), Is.LessThan(1e-3M));
@@ -58,7 +59,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_WoodWithDoubleLowLengthF1A_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("ELA");
+            var p = new ELA();
             var f1a = new F1A(1000L, 1000L);
             var price = p.GetMaterialPrice(f1a);
             Assert.That(Math.Abs(price - (985 + 38) * 1.5M), Is.LessThan(1e-3M));
@@ -67,7 +68,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_WoodWithDoubleLowLengthF2A_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("ELA");
+            var p = new ELA();
             var f2a = new F2A(1000L, 1000L);
             var price = p.GetMaterialPrice(f2a);
             Assert.That(Math.Abs(price - (985 + 38) * 1.8M), Is.LessThan(1e-3M));
@@ -76,7 +77,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_RALTWithDoubleAllowedLength_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("RALT");
+            var p = new RALT();
             var f1a = new F1A(5000L, 4000L);
             var price = p.GetMaterialPrice(f1a);
             Assert.That(Math.Abs(price - (842 + 38) * 5M * 4M), Is.LessThan(1e-3M));
@@ -85,7 +86,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_AALAMWithDoubleAllowedLength_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("AALAM");
+            var p = new AALAM();
             var f1a = new F1A(5000L, 4000L);
             var price = p.GetMaterialPrice(f1a);
             Assert.That(Math.Abs(price - (876 + 38) * 5M * 4M), Is.LessThan(1e-3M));
@@ -94,7 +95,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_AATTWithDoubleAllowedLength_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("AATT");
+            var p = new AATT();
             var f1a = new F1A(5000L, 4000L);
             var price = p.GetMaterialPrice(f1a);
             Assert.That(Math.Abs(price - (694 + 38) * 5M * 4M), Is.LessThan(1e-3M));
@@ -103,7 +104,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_IPCWithDoubleAllowedLength_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("IPC");
+            var p = new IPC();
             var f1a = new F1A(5000L, 4000L);
             var price = p.GetMaterialPrice(f1a);
             Assert.That(Math.Abs(price - (528 + 38) * 5M * 4M), Is.LessThan(1e-3M));
@@ -112,7 +113,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_IPNWithDoubleAllowedLength_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("IPN");
+            var p = new IPN();
             var f1a = new F1A(5000L, 4000L);
             var price = p.GetMaterialPrice(f1a);
             Assert.That(Math.Abs(price - (528 + 38) * 5M * 4M), Is.LessThan(1e-3M));
@@ -121,7 +122,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_SPWithFIXALowLength_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("SP");
+            var p = new SP();
             var f1a = new F1A(1500L, 1200L);
             var price = p.GetMaterialPrice(f1a);
             Assert.That(Math.Abs(price - (635 + 38) * 1.5M * 1.2M), Is.LessThan(1e-3M));
@@ -130,7 +131,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_ELAWithFixed_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("ELA");
+            var p = new ELA();
             var fix = new FIX(1800L, 1350L);
             var price = p.GetMaterialPrice(fix);
             Assert.That(Math.Abs(price - (568 + 38) * 1.8M * 1.35M), Is.LessThan(1e-3M));
@@ -139,7 +140,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_SLFWithFixed_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("RALT");
+            var p = new RALT();
             var slf = new SLF(1120L, 2400L);
             var price = p.GetMaterialPrice(slf);
             Assert.That(Math.Abs(price - (526 + 38) * 1.12M * 2.4M), Is.LessThan(1e-3M));
@@ -147,7 +148,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_AATTWithFixed_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("AATT");
+            var p = new AATT();
             var fld = new FLD(1780L, 1500L);
             var price = p.GetMaterialPrice(fld);
             Assert.That(Math.Abs(price - (501 + 38) * 1.78M * 1.5M), Is.LessThan(1e-3M));
@@ -156,7 +157,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_IPCWithFixed_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("IPC");
+            var p = new IPC();
             var fls = new FLS(1300L, 1800L);
             var price = p.GetMaterialPrice(fls);
             Assert.That(Math.Abs(price - (385 + 38) * 1.3M * 1.8M), Is.LessThan(1e-3M));
@@ -165,7 +166,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_COPWithPvc_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("IPC");
+            var p = new IPC();
             var cop = new COP(3000L);
             var price = p.GetMaterialPrice(cop);
             Assert.That(Math.Abs(price - 6 * 3M), Is.LessThan(1e-3M));
@@ -174,7 +175,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_COPWithPvcLowLength_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("IPC");
+            var p = new IPC();
             var cop = new COP(100L);
             var price = p.GetMaterialPrice(cop);
             Assert.That(Math.Abs(price - 6 * 0.1M), Is.LessThan(1e-3M));
@@ -183,7 +184,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_COPWithWood_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("AATT");
+            var p = new AATT();
             var cop = new COP(3000L);
             var price = p.GetMaterialPrice(cop);
             Assert.That(Math.Abs(price - 18.2M * 3M), Is.LessThan(1e-3M));
@@ -192,7 +193,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_COPWithWoodLowLength_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("AATT");
+            var p = new AATT();
             var cop = new COP(100L);
             var price = p.GetMaterialPrice(cop);
             Assert.That(Math.Abs(price - 18.2M * 0.1M), Is.LessThan(1e-3M));
@@ -201,7 +202,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_FROWithWood_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("AATT");
+            var p = new AATT();
             var fro = new FRO(3000L);
             var price = p.GetMaterialPrice(fro);
             Assert.That(Math.Abs(price - 142M * 3M), Is.LessThan(1e-3M));
@@ -210,7 +211,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_FROWithWoodLowLength_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("AATT");
+            var p = new AATT();
             var fro = new FRO(100L);
             var price = p.GetMaterialPrice(fro);
             Assert.That(Math.Abs(price - 142M * 1M), Is.LessThan(1e-3M));
@@ -219,7 +220,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_FROWithPvc_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("IPC");
+            var p = new IPC();
             var fro = new FRO(3000L);
             var price = p.GetMaterialPrice(fro);
             Assert.That(Math.Abs(price - 142M * 3M), Is.LessThan(1e-3M));
@@ -228,7 +229,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_FROWithPvcLowLength_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("IPC");
+            var p = new IPC();
             var fro = new FRO(100L);
             var price = p.GetMaterialPrice(fro);
             Assert.That(Math.Abs(price - 142M * 1M), Is.LessThan(1e-3M));
@@ -237,7 +238,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_PRT1AWithWoodAntaMax_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("AALAM");
+            var p = new AALAM();
             var prt1a = new PRT1A(3000L, 4500L);
             var price = p.GetMaterialPrice(prt1a);
             var expected = (876M + 38M) * 3M * 4.5M + 616M;
@@ -247,7 +248,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_PRT1AWithWoodAntaMaxLowArea_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("AALAM");
+            var p = new AALAM();
             var prt1a = new PRT1A(1000L, 1000L);
             var price = p.GetMaterialPrice(prt1a);
             var expected = (876M + 38M) * 1.5M + 616M;
@@ -257,7 +258,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_PRT2AWithWoodAntaMax_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("AALAM");
+            var p = new AALAM();
             var prt2a = new PRT2A(3000L, 4500L);
             var price = p.GetMaterialPrice(prt2a);
             var expected = (876M + 38M) * 3M * 4.5M + 616M;
@@ -267,7 +268,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_PRT2AWithWoodAntaMaxLowArea_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("AALAM");
+            var p = new AALAM();
             var prt2a = new PRT2A(1000L, 1000L);
             var price = p.GetMaterialPrice(prt2a);
             var expected = (876M + 38M) * 1.8M + 616M;
@@ -277,7 +278,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_PRT1AWithPvcAntaMax_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("IPCAM");
+            var p = new IPCAM();
             var prt1a = new PRT1A(3000L, 4500L);
             var price = p.GetMaterialPrice(prt1a);
             var expected = (635M + 38M) * 3M * 4.5M + 616M;
@@ -287,7 +288,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_PRT1AWithPvcAntaMaxLowArea_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("IPCAM");
+            var p = new IPCAM();
             var prt1a = new PRT1A(1000L, 1000L);
             var price = p.GetMaterialPrice(prt1a);
             var expected = (635M + 38M) * 1.5M + 616M;
@@ -297,7 +298,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_PRT2AWithPvcAntaMax_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("IPCAM");
+            var p = new IPCAM();
             var prt2a = new PRT2A(3000L, 4500L);
             var price = p.GetMaterialPrice(prt2a);
             var expected = (635M + 38M) * 3M * 4.5M + 616M;
@@ -307,7 +308,7 @@ namespace DomainModel_Test
         [Test]
         public void Test_PRT2AWithPvcAntaMaxLowArea_PriceIsCorrect()
         {
-            var p = ProductFactory.CreateByCode("IPCAM");
+            var p = new IPCAM();
             var prt2a = new PRT2A(1000L, 1000L);
             var price = p.GetMaterialPrice(prt2a);
             var expected = (635M + 38M) * 1.8M + 616M;
