81a5e43e44409083b1f1fc3c906d307e5a12de7e
Marcello Esposito <esposito.marce@gmail.com>
2025-03-14 20:57:59 +0100

    fix(backend): class names typos fixed

--- src/backend/src/DomainModel/Classes/Colors/ConcreteColors/BrushcedAsh1013.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DomainModel.Classes.Colors.ConcreteColors
-{
-    public class BrushcedAsh1013 : WoodColor { public override string Description => "Frassino Spazzolato 1013"; public override decimal Price_mq => 84M; public override int Order => 300; }
-
-
-}
--- src/backend/src/DomainModel/Classes/Colors/ConcreteColors/MedimuColoredAsh.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace DomainModel.Classes.Colors.ConcreteColors
-{
-    public class MedimuColoredAsh : WoodColor { public override string Description => "Frassinto Tinto Medio"; public override decimal Price_mq => 0M; public override int Order => 220; }
-
-}
