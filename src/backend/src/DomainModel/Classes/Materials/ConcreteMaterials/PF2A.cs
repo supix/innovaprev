7feb68d849d02ab2434c6c5fa896095ef41cff66
Marcello Esposito <esposito.marce@gmail.com>
2025-05-12 22:06:19 +0200

    fix(backend): PF2A material description is correct

--- src/backend/src/DomainModel/Classes/Materials/ConcreteMaterials/PF2A.cs
+++ src/backend/src/DomainModel/Classes/Materials/ConcreteMaterials/PF2A.cs
@@ -5,7 +5,7 @@
         public PF2A(long height_mm, long width_mm, string openingType, bool opaqueGlass, bool wireCover) : base(height_mm, width_mm, openingType, opaqueGlass, wireCover)
         {
         }
-        public override string Description => "Finestra 2 Ante";
+        public override string Description => "Portafinestra 2 Ante";
         protected override long? ClampMinValue => 1800000;
         public override int Order => 70;
         public override string[] MaterialForProduct => base.GetNotAntaMaxAndNotScorrevoleProductCodes();
