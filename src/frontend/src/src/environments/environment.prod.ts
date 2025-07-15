1bf1819a5bc0dab81a02c28aec5af4cb76d79ea2
Davide Cappa <cappadavide@gmail.com>
2025-07-15 22:44:15 +0200

    fix(frontend): remove app.config.prod replacement to avoid missing toastr providers in prod

--- src/frontend/src/src/environments/environment.prod.ts
+++ src/frontend/src/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 import {sharedEnvironment} from './environment.shared';
 
 export const environment = {
+  enableMockInterceptor: false,
   production: true,
   enableFillFormButton: false,
   ...sharedEnvironment
