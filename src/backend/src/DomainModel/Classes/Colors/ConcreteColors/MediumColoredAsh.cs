19b3096938413b4e4805330256a75f08c1c2ef82
Marcello Esposito <esposito.marce@gmail.com>
2025-09-13 18:18:14 +0200

    fix(backend): type fixed

--- src/backend/src/DomainModel/Classes/Colors/ConcreteColors/MediumColoredAsh.cs
+++ src/backend/src/DomainModel/Classes/Colors/ConcreteColors/MediumColoredAsh.cs
@@ -2,7 +2,7 @@
 {
     public class MediumColoredAsh : StandardWoodColor
     {
-        public override string Description => "Frassinto Tinto Medio";
+        public override string Description => "Frassino Tinto Medio";
         public override decimal Price_sqm => 0M;
         public override int Order => 220;
     }
