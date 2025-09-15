6430c2ba8ca6e3bb360ca6661ec381194091a230
Marcello Esposito <esposito.marce@gmail.com>
2025-09-15 19:41:12 +0200

    fix(backend): fix FIX material description

--- src/backend/src/DomainModel/Classes/Materials/ConcreteMaterials/FIX.cs
+++ src/backend/src/DomainModel/Classes/Materials/ConcreteMaterials/FIX.cs
@@ -7,7 +7,7 @@ namespace DomainModel.Classes.Materials.ConcreteMaterials
         public FIX(long height_mm, long width_mm, string openingType, bool opaqueGlass, bool wireCover, IFrame frameType) : base(height_mm, width_mm, openingType, opaqueGlass, wireCover, frameType)
         {
         }
-        public override string Description => "Fisso con fermavetro";
+        public override string Description => "Fisso";
         protected override long? ClampMinValue => 1000000;
         public override int Order => 0;
         public override string[] MaterialForProduct => base.GetNotAntaMaxAndNotScorrevoleProductCodes();
