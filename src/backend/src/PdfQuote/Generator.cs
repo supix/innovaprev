641c313e47ee923d6a0129e97b04b38f30e77af5
Marcello Esposito <esposito.marce@gmail.com>
2025-11-04 18:37:05 +0100

    feat(backend): quotation has static heading pages
    The heading is deployed as a file in the target compilation directory.

--- src/backend/src/PdfQuote/Generator.cs
+++ src/backend/src/PdfQuote/Generator.cs
@@ -10,6 +10,7 @@ namespace PdfQuote
 {
     public class Generator : IPdfReportGenerator
     {
+        private static Random random = new Random();
         private readonly IPriceCalculator priceCalculator;
         private readonly ICollectionProvider collectionProvider;
         private readonly IProductImageProvider imageProvider;
@@ -28,7 +29,41 @@ namespace PdfQuote
             QuestPDF.Settings.License = LicenseType.Community;
             var priceInfo = priceCalculator.getPrices(project.ProductData, project.WindowsData, project.CustomData);
             var doc = new QuoteTemplate(project, priceInfo, collectionProvider, imageProvider, wir);
-            return Document.Create(container => doc.Compose(container)).GeneratePdf();
+            var quotation = Document.Create(container => doc.Compose(container)).GeneratePdf();
+            var quotationFileName = GetRandomFileName();
+            var fullQuotationFileName = GetRandomFileName();
+            try
+            {
+                using (var s = new FileStream(quotationFileName, FileMode.CreateNew))
+                {
+                    s.Write(quotation);
+                }
+                var headingFile = Path.Combine(AppContext.BaseDirectory, "heading.pdf");
+                DocumentOperation
+                    .LoadFile(headingFile)
+                    .MergeFile(quotationFileName)
+                    .Save(fullQuotationFileName);
+                byte[] result = File.ReadAllBytes(fullQuotationFileName);
+                return result;
+            }
+            finally
+            {
+                File.Delete(quotationFileName);
+                File.Delete(fullQuotationFileName);
+            }
+        }
+
+        private string GetRandomFileName()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var fileName = new char[20];
+
+            for (int i = 0; i < fileName.Length; i++)
+            {
+                fileName[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new string(fileName) + ".pdf";
         }
     }
 }
